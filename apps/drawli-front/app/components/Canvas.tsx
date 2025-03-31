f81fe4859c42669d5e7250e3e2105449977297df
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-03-31 17:23:11 +0530

    icons

--- apps/drawli-front/app/components/Canvas.tsx
+++ apps/drawli-front/app/components/Canvas.tsx
@@ -1,18 +1,80 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { InitDraw } from "../draw";
+import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
 
-export function Canvas({roomId , socket}:{roomId: string , socket:WebSocket}) {
+type Shape = "circle" | "rect" | "pencil";
+export function Canvas({
+  roomId,
+  socket,
+}: {
+  roomId: string;
+  socket: WebSocket;
+}) {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [selectedTool, setSelectedTool] = useState<Shape >("circle");
+  useEffect(() => {
+    if (canvasRef.current) {
+      InitDraw(canvasRef.current, roomId, socket);
+    }
+  }, [roomId, socket]);
+  console.log("Received roomId in Canvas:", roomId);
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "black",
+        overflow: "hidden",
+      }}
+    >
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      ></canvas>
+      <Topbar selectedTool={selectedTool} setSelectedTool={setSelectedTool} />
+    </div>
+  );
+}
 
-     const canvasRef = useRef<HTMLCanvasElement>(null);
-     useEffect(() => {
-        if (canvasRef.current) {
-            InitDraw(canvasRef.current , roomId , socket);
-        }
-    }, [roomId, socket]);
-    console.log("Received roomId in Canvas:", roomId);
-    return (
-        <div>
-            <canvas ref={canvasRef} width={10000} height={10000} ></canvas> 
-        </div>
-    );
+function Topbar({
+  selectedTool,
+  setSelectedTool,
+}: {
+  selectedTool: Shape;
+  setSelectedTool: (s: Shape) => void;
+}) {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 10,
+        left: 10,
+      }}
+    >
+      <div className="flex gap-t">
+        <IconButton
+          onClick={() => {
+            setSelectedTool("pencil");
+          }}
+          activated={selectedTool === "pencil"}
+          icon={<Pencil />}
+        />
+        <IconButton
+          onClick={() => {
+            setSelectedTool("rect");
+          }}
+          activated={selectedTool === "rect"}
+          icon={<RectangleHorizontalIcon />}
+        ></IconButton>
+        <IconButton
+          onClick={() => {
+            setSelectedTool("circle");
+          }}
+          activated={selectedTool === "circle"}
+          icon={<Circle />}
+        ></IconButton>
+      </div>
+    </div>
+  );
 }
